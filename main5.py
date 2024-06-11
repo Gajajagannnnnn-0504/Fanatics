916b1459fb79748fd95fd8d5d0eb990461459ccc
Gaja_jagan <133838667+Gajajagannnnnn-0504@users.noreply.github.com>
2024-06-11 10:44:53 +0530

    Add files via upload

--- main5.py
+++ main5.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 
-st.title("22AIA-WEBSPIRITS")
+st.title("22AIA-FANATICS")
 # Title and introduction
 st.title("Naive Bayes Classifier for IMDb Review Classification")
 st.write("This app uses a Naive Bayes classifier to predict whether an IMDb review is positive or negative.")
